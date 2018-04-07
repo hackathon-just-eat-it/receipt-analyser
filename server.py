6cb09a42b6c53782a8d65beb1c9ea71016813122
samutamm <samutamm@gmail.com>
2018-04-07 22:56:37 +0200

    add images

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 
 from flask import Flask, jsonify, request
+import pandas as pd
 from UserModelDB import UserModelDB
 
 app = Flask(__name__)
 
 models = UserModelDB()
+images = pd.read_csv('receipt_images.csv')
 
 @app.route("/")
 def hello():
@@ -16,6 +18,11 @@ def recomendation():
     usermodel = models.get_user_model(user)
     
     # ML MAGIC
-    result = [] #?
+    result = [
+        {'id':'Mushroom-risotto-352492'}
+    ] #?
+    
+    for line in result: 
+        line['images'] = images[images['id'] == line['id']]['images'].values[0]
     
     return jsonify(result)
